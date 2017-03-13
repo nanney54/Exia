404339a7340a6e11474318e8f67d8a243fd9b2a4
nanney54 <nanney54@users.noreply.github.com>
2017-03-14 00:52:02 +0100

    Importation ExpressionExtension.

---
+++ src/Platform/Desktop/Exia.Utils/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Exia.Utils")]
+[assembly: AssemblyProduct("Exia.Utils")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6fbdfdd8-812a-4c1f-8da8-ce451c597eab")]
---
+++ src/Shared/Shared.Exia.Utils/ExpressionExtension.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Exia.Utils {
+    public static class ExpressionExtension {
+        /// <summary>
+        /// Get the name of a specified property lambda expression
+        /// </summary>
+        /// <param name="propertyExpression">A lambda expression</param>
+        /// <returns>A string containing the name of the property</returns>
+        public static string GetPropertyName(LambdaExpression propertyExpression) {
+            if (propertyExpression == null) {
+                throw new ArgumentNullException("propertyExpression");
+            }
+
+            MemberExpression body = propertyExpression.Body as MemberExpression;
+
+            if (body == null) {
+                throw new ArgumentException("Invalid argument", "propertyExpression");
+            }
+
+            PropertyInfo member = body.Member as PropertyInfo;
+
+            if (member == null) {
+                throw new ArgumentException("Argument is not a property", "propertyExpression");
+            }
+
+            return member.Name;
+        }
+
+        /// <summary>
+        /// Get the name of a specified property lambda expression
+        /// </summary>
+        /// <typeparam name="T">Type of the property</typeparam>
+        /// <param name="propertyExpression">A lambda expression</param>
+        /// <returns>A string containing the name of the property</returns>
+        public static string GetPropertyName<T>(Expression<Func<T>> propertyExpression) {
+            return ExpressionExtension.GetPropertyName(propertyExpression as LambdaExpression);
+        }
+
+        /// <summary>
+        /// Compare a string to a property expression
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="propertyExpression"></param>
+        /// <returns></returns>
+        public static bool EqualToPropertyName<T>(this string source, Expression<Func<T>> propertyExpression) {
+            if (string.IsNullOrEmpty(source)) {
+                throw new ArgumentNullException("Source");
+            }
+
+            if (propertyExpression == null) {
+                throw new ArgumentNullException("propertyName");
+            }
+
+            string propertyName = ExpressionExtension.GetPropertyName(propertyExpression);
+
+            return source == propertyName;
+        }
+    }
+}
